a4a9deff6c6db94e2156257237ec1060be9d273e
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-09-20 20:36:38 -0300

    feat: implementa gerenciamento de serviços para prestadores

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
-
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   resource :session, only: [:new, :create, :destroy]
   resource :provider, only: [:new, :create, :edit, :update]
+  resources :services, only: [:index, :new, :create]
 
   get "pages/home"
-
   root "pages#home"
 end
